8c8bbc17150b5869c4060635a4b8112f8a4f6d55
Jeremie Shaker <jeremie.shaker@hotmail.com>
2017-03-12 22:34:13 -0400

    added body parser for parsing POST request

--- api/getPlaces.js
+++ api/getPlaces.js
@@ -1,7 +1,8 @@
 import https from 'https';
 
 export default function(req,res){
-  https.get(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${req.params.searchText}&&key=AIzaSyCp6lahESZxAp0LSXxyUrWXFy1KkGICDws`, function(response){
+
+  https.get(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${req.query.search}&&key=AIzaSyCp6lahESZxAp0LSXxyUrWXFy1KkGICDws`, function(response){
 
     var body = '';
     response.on('data', function(d) {
