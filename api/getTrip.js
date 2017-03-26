169e7d93e42b8951d021b875d7a0793431f781e8
patch100 <patch.93@gmail.com>
2017-03-26 10:17:09 -0400

    added destination to response object

--- api/getTrip.js
+++ api/getTrip.js
@@ -8,11 +8,13 @@ export default function(req,res){
   const nearestAirportReq = {latitude, longitude};
   getDestination(req.body.destination).then(function(destination) {
     const destinationReq = {lat: destination.geometry.location.lat, long: destination.geometry.location.lng, name: destination.name};
+    response.destination = destinationReq;
     getNearestAirport(nearestAirportReq).then(function(nearestAirport){
-    const directionsReq = {userLatitude: latitude,userLongitude: longitude, airportLatitude: nearestAirport.geometry.location.lat, airportLongitude: nearestAirport.geometry.location.lng};
-    getDirectionsToAirport(directionsReq).then(function(airportDirections){
-      response.airportDirections = airportDirections;
-      res.send(response);
+      const directionsReq = {userLatitude: latitude,userLongitude: longitude, airportLatitude: nearestAirport.geometry.location.lat, airportLongitude: nearestAirport.geometry.location.lng};
+      getDirectionsToAirport(directionsReq).then(function(airportDirections){
+        response.airportDirections = airportDirections;
+        res.send(response);
+      });
     });
   });
-};
+}
