820298e63ec57ae5ed37ec5f01044d3440bf2346
patch100 <patch.93@gmail.com>
2017-03-26 10:07:41 -0400

    Added promise for trip destination

--- api/getTrip.js
+++ api/getTrip.js
@@ -1,11 +1,14 @@
 import getNearestAirport from './getNearestAirport';
 import getDirectionsToAirport from './getDirectionsToAirport';
+import getDestination from './getDestination';
 
 export default function(req,res){
   let response = {};
   const {latitude, longitude} = req.body.userLocation;
   const nearestAirportReq = {latitude, longitude};
-  getNearestAirport(nearestAirportReq).then(function(nearestAirport){
+  getDestination(req.body.destination).then(function(destination) {
+    const destinationReq = {lat: destination.geometry.location.lat, long: destination.geometry.location.lng, name: destination.name};
+    getNearestAirport(nearestAirportReq).then(function(nearestAirport){
     const directionsReq = {userLatitude: latitude,userLongitude: longitude, airportLatitude: nearestAirport.geometry.location.lat, airportLongitude: nearestAirport.geometry.location.lng};
     getDirectionsToAirport(directionsReq).then(function(airportDirections){
       response.airportDirections = airportDirections;
