66d477190c6269d6cb5fb7f01d4d40213b9d5349
patch100 <patch.93@gmail.com>
2017-03-26 12:22:41 -0400

    Seperated api calls into two seperate chains destination information now includes destination airport

--- api/getTrip.js
+++ api/getTrip.js
@@ -1,20 +1,53 @@
 import getNearestAirport from './getNearestAirport';
 import getDirectionsToAirport from './getDirectionsToAirport';
 import getDestination from './getDestination';
+import Q from 'q';
 
 export default function(req,res){
-  let response = {};
   const {latitude, longitude} = req.body.userLocation;
   const nearestAirportReq = {latitude, longitude};
-  getDestination(req.body.destination).then(function(destination) {
-    const destinationReq = {lat: destination.geometry.location.lat, long: destination.geometry.location.lng, name: destination.name};
-    response.destination = destinationReq;
-    getNearestAirport(nearestAirportReq).then(function(nearestAirport){
-      const directionsReq = {userLatitude: latitude,userLongitude: longitude, airportLatitude: nearestAirport.geometry.location.lat, airportLongitude: nearestAirport.geometry.location.lng};
-      getDirectionsToAirport(directionsReq).then(function(airportDirections){
-        response.airportDirections = airportDirections;
-        res.send(response);
-      });
+
+  let directionsPromise = getNearestAirport(nearestAirportReq).then(function(nearestAirport){
+
+    const directionsReq = {
+      userLatitude: latitude,
+      userLongitude: longitude,
+      airportLatitude: nearestAirport.geometry.location.lat,
+      airportLongitude: nearestAirport.geometry.location.lng
+    };
+
+    return getDirectionsToAirport(directionsReq).then(function(airportDirections){
+      directionsReq.routes = airportDirections;
+      return directionsReq;
     });
+  });
+
+  let destinationPromise = getDestination(req.body.destination).then(function(destination) {
+    const flightTicketReq = {
+      latitude: destination.geometry.location.lat,
+      longitude: destination.geometry.location.lng
+    };
+
+    return getNearestAirport(flightTicketReq).then(function(nearestAirport) {
+      flightTicketReq.airport = {
+        lat: nearestAirport.geometry.location.lat,
+        lng: nearestAirport.geometry.location.lng,
+        name: nearestAirport.name
+      };
+      return flightTicketReq;
+    });
+  });
+
+  let result = Q.all([directionsPromise, destinationPromise]);
+
+  result.spread(function (directions, destination) {
+    console.log(directions);
+    console.log(destination);
+
+    let response = {};
+    response.directions = directions;
+    response.destination = destination;
+
+    res.send(response);
   });
 }
