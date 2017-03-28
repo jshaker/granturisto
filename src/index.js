10a27046b72d2225aa2fc8721c9e75eae29df50b
Jeremie Shaker <jeremie.shaker@hotmail.com>
2017-03-28 15:28:19 -0400

    changed stylesheet to eliminate padding

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import './styles.css';
 
 injectTapEventPlugin();
 render(
